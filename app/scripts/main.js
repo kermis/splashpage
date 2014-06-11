30eb35d4bacabf5c05828b4003bcd08529f65ccf
Robbert Wolfs <robbertwolfs@hotmail.com>
2014-06-11 11:21:07 +0200

    dont play video on mobile

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(function() {
+
+      if ($('#header').css('display') === 'none') {
+            // $('#header').remove();
+
+            console.log('pause');
+            $('#video').get(0).pause();
+
+
+      }
+
+});
